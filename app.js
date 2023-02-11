03515fc9ffd01e2c3452331b4905236180e81a4f
Artamis <artam1s@protonmail.com>
2023-02-10 23:33:01 -0600

    Finished adding functionalities

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const path = require("path")
 const express = require("express")
+const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const morgan = require("morgan")
-const handlebars = require("express-handlebars").create({ defaultLayout: "main"})
+const { formatDate,stripTags,truncate, editIcon,select } = require("./helpers/hbs")
+const handlebars = require("express-handlebars").create({helpers:{formatDate,stripTags,truncate,editIcon,select},defaultLayout: "main"})
+const methodOverride = require("method-override")
 const passport = require("passport")
 const session = require("express-session")
+const MongoStore = require('connect-mongo')
 const connectDB = require("./config/db.js")
 
 // Load config
@@ -17,13 +21,29 @@ connectDB();
 
 const app = express();
 
+// Body parser
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+// Method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  })
+)
+
 // Logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
 // Handlebars
-app.engine('handlebars', handlebars.engine)
+app.engine('handlebars',handlebars.engine)
 //app.engine(".hbs", engine({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".handlebars");
 app.set('views', './views');
@@ -33,6 +53,7 @@ app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: false,
+  store: MongoStore.create({mongooseConnection: mongoose.connection,mongoUrl: process.env.MONGO_URI}),
 }))
 
 
@@ -40,12 +61,19 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
+// Set global var
+app.use(function (req,res,next){
+  res.locals.user = req.user || null
+  next()
+})
+
 // Static folder
 app.use(express.static(path.join(__dirname,"public")))
 
 // Routes
 app.use("/",require("./routes/index"))
 app.use("/auth",require("./routes/auth"))
+app.use("/stories",require("./routes/stories"))
 
 const PORT = process.env.PORT || 3000;
 app.listen(
